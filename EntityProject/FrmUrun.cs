0fc144c40155c6388c843c9025d0df47c65aeea5
Hüseyin <91679832+huseyinoztrk@users.noreply.github.com>
2023-03-29 21:43:16 +0300

    Upload Files

---
+++ EntityProject/FrmUrun.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityProject
+{
+    public partial class FrmUrun : Form
+    {
+        public FrmUrun()
+        {
+            InitializeComponent();
+        }
+        DbEntityUrunEntities db = new DbEntityUrunEntities();
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = (from x in db.TBLURUN
+                                        select new
+                                        {
+                                            x.URUNID,
+                                            x.URUNAD,
+                                            x.MARK,
+                                            x.STOK,
+                                            x.FİYAT,
+                                            x.TBLKATEGORI.AD,
+                                            x.DURUM
+                                        }).ToList();
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TBLURUN t = new TBLURUN();
+            t.URUNAD = TxtUrunAdi.Text;
+            t.MARK = TxtMarka.Text;
+            t.STOK = short.Parse(TxtStok.Text);
+            t.KATEGORI = int.Parse(comboBox1.Text);
+            t.FİYAT = decimal.Parse(TxtFiyat.Text);
+            t.DURUM = true;
+            db.TBLURUN.Add(t);
+            db.SaveChanges();
+            MessageBox.Show("Ürün listeye eklendi.");
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtID.Text);
+            var urun = db.TBLURUN.Find(x);
+            db.TBLURUN.Remove(urun);
+            db.SaveChanges();
+            MessageBox.Show("Ürün silinmiştir.");
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtID.Text);
+            var urun = db.TBLURUN.Find(x);
+            urun.URUNAD = TxtUrunAdi.Text;
+            urun.MARK = TxtMarka.Text;
+            urun.STOK = short.Parse(TxtStok.Text);
+            urun.FİYAT = int.Parse(TxtFiyat.Text);
+            db.SaveChanges();
+            MessageBox.Show("Ürün güncellenmiştir.");
+        }
+
+        private void FrmUrun_Load(object sender, EventArgs e)
+        {
+            var kategoriler = (from x in db.TBLKATEGORI
+                               select new
+                               {
+                                   x.ID,
+                                   x.AD
+                               }).ToList();
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "AD";
+            comboBox1.DataSource = kategoriler;
+        }
+
+        private void BtnTemizle_Click(object sender, EventArgs e)
+        {
+            TxtID.Text = "";
+            TxtUrunAdi.Text = "";
+            TxtMarka.Text = "";
+            TxtStok.Text = "";
+            TxtFiyat.Text = "";
+            TxtDurum.Text = "";
+        }
+    }
+}
